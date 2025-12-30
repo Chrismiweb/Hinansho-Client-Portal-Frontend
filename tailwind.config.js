593cb5d99592853dea3078bb8e16848199d0b36c
Coded76 <0xhope66@gmail.com>
2025-12-30 18:29:11 +0100

    Register and Login button

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)", "sans-serif"],
+        pop: ["var(--font-poppins)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
