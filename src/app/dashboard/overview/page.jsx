916d7119c41f7b100b668e9bd70eff847a81d4f5
chrismiWeb <chrismibiteso@gmail.com>
2026-01-07 22:26:40 +0100

    Initial dashboard layout and components

---
+++ src/app/dashboard/overview/page.jsx
@@ -0,0 +1,12 @@
+import OverviewPage from '@/components/dashboard/overview/OverviewPage'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <OverviewPage/>
+    </div>
+  )
+}
+
+export default page
